df984b7c12cccf8ffd72adc5dbad879d21663422
M.Haris Khan <hkhan@cedrus.com>
2023-07-27 12:32:06 +0500

    Feat;StringMasking

---
+++ StringMasking/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Program
+{
+    public static string Maskify(string input)
+    {
+        if (string.IsNullOrEmpty(input) || input.Length <= 4)
+            return input;
+
+        char[] maskedChars = input.ToCharArray();
+        for (int i = 0; i < input.Length - 4; i++)
+        {
+            maskedChars[i] = '#';
+        }
+
+        return new string(maskedChars);
+
+    }
+    public static void Main(string[] args)
+    {
+        String Text;
+        Console.WriteLine("Enter a string");
+        Text = Convert.ToString(Console.ReadLine());
+        string value = Maskify(Text);
+        Console.WriteLine(value);
+        Console.ReadLine();
+
+    }
+}
